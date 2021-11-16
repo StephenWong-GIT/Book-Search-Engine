eb77871b17a744b4de13054584a6782dba9b3730
Stephen Wong <yummywongttv@gmail.com>
2021-11-15 19:35:28 -0500

    deploying to heroku

---
+++ client/build/precache-manifest.91b8d26478d547b1fa1103bb0753d385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373ba33884bc2d4458bb2314b11ebca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "137e9c0cdcd5f28a1b07",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "281cac13a9d1586b53d8",
+    "url": "/static/css/main.18ac4871.chunk.css"
+  },
+  {
+    "revision": "137e9c0cdcd5f28a1b07",
+    "url": "/static/js/2.61d01a22.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.61d01a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281cac13a9d1586b53d8",
+    "url": "/static/js/main.fba8c479.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
